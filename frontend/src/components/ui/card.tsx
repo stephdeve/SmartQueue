9d1164422bac6b8e7a5f6f6b88c251058b95797c
Bestbeedev <josueaoga0@gmail.com>
2026-06-10 10:24:43 +0100

    remove shadow-lg

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -13,7 +13,7 @@ const Card = React.forwardRef<HTMLDivElement, CardProps>(
     <div
       ref={ref}
       className={cn(
-        "rounded-lg border bg-card text-card-foreground shadow-lg",
+        "rounded-lg border bg-card text-card-foreground shadow-none",
         className
       )}
       {...props}
